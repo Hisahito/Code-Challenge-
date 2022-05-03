919a53f4194685def914e63d921e8ee9626495ab
hisahito <hisahito706@gmail.com>
2022-05-02 22:34:15 -0500

    2do try to run auto tests in github actions OK

--- tests/utils/FileReader.test.js
+++ tests/utils/FileReader.test.js
@@ -2,7 +2,7 @@ const Reader = require("./../../lib/utils/FileReader");
 
 describe("Test in FileReader.js File", () => {
     test("should return an object", () => {
-        const fileReader = Reader.readJsonFile("visualPartners.json");
+        const fileReader = Reader.readJsonFile("visualpartners.json");
         expect(fileReader).toBeInstanceOf(Object);
     });
 
